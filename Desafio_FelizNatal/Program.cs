75de223726844aca0939e58c8d010db5a1929104
Jean Leonardo Soares de Moraes <jean.2018130570@unicap.br>
2021-10-05 10:26:57 -0300

    Add project files.

---
+++ Desafio_FelizNatal/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class TESTE
+{
+
+    static void Main(string[] args)
+    {
+
+        int i;
+        int x = int.Parse(Console.ReadLine());
+        Console.Write("Feliz nat");
+        for (i = 0; i < x; i++)
+        {
+            Console.Write("a");
+        }
+        Console.Write("l!\n");
+
+    }
+
+}
